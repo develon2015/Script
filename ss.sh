c511abfb5e7a38159f9d84d7852878892c81d673
Long Yong <302615249@qq.com>
2019-05-25 23:37:45 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,4 +1,5 @@
 wget https://github.com/develon2015/Mosh-UDP/blob/master/server?raw=true -O ss && chmod +x ./ss && mv ./ss /usr/bin/ss
 echo '/usr/bin/ss 6666 ss' >> /etc/rc.local
 /usr/bin/ss 6666 ss
+clear
 echo 'Install succeed'
